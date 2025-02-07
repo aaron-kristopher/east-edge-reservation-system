22d9a1ba01c72f387d117af32bfe1e3b4298ef93
Aaron Lim <aaron.lim.cstr@gmail.com>
2025-02-07 22:13:35 +0800

    refactor: Update model __str__ for readability

--- east_edge_reservation/customers/models.py
+++ east_edge_reservation/customers/models.py
@@ -7,3 +7,6 @@ class Customer(models.Model):
     phone_number = models.CharField(max_length=11)
     email = models.EmailField(max_length=254)
     password = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
