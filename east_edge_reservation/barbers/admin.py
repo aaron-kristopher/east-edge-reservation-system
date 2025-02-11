38df999d2a2ea4341f6403fd1b9b3cabc69cc598
Aaron Lim <aaron.lim.cstr@gmail.com>
2025-02-11 09:03:52 +0800

    refactor: Change from junction table to ManyToMany relationship

--- east_edge_reservation/barbers/admin.py
+++ east_edge_reservation/barbers/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Barber, Service, BarberService
+from .models import Barber, Service
 
 # Register your models here.
 admin.site.register(Barber)
 admin.site.register(Service)
-admin.site.register(BarberService)
--- east_edge_reservation/barbers/models.py
+++ east_edge_reservation/barbers/models.py
@@ -5,6 +5,9 @@ class Barber(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=11)
+    services = models.ManyToManyField(
+        "Service", verbose_name="Services Offered", related_name="barbers"
+    )
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
@@ -17,25 +20,3 @@ class Service(models.Model):
 
     def __str__(self):
         return f"{self.service_name} - {self.price} PHP ({self.estimated_time} mins)"
-
-
-class BarberService(models.Model):
-    barber = models.ForeignKey(
-        Barber,
-        on_delete=models.CASCADE,
-        verbose_name="Barber",
-        related_name="services_offered",
-    )
-
-    service = models.ForeignKey(
-        Service,
-        on_delete=models.CASCADE,
-        verbose_name="Service",
-        related_name="barbers",
-    )
-
-    def __str__(self):
-        return f"{self.barber.first_name} {self.barber.last_name} - {self.service.service_name}"
-
-    class Meta:
-        unique_together = ("barber", "service")
