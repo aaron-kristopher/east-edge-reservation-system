b15ef2cb7d35f0443e02c7a601bf8655d3fbffa1
Aaron Lim <aaron.lim.cstr@gmail.com>
2025-02-12 15:13:20 +0800

    feat: Add filter and action to schedule

--- east_edge_reservation/barbers/admin.py
+++ east_edge_reservation/barbers/admin.py
@@ -1,7 +1,15 @@
 from django.contrib.admin import register
 from django.utils.translation import gettext_lazy as _
+from django.utils.timezone import now, timedelta
 from unfold.admin import ModelAdmin
-from unfold.contrib.filters.admin import MultipleRelatedDropdownFilter
+from unfold.contrib.filters.admin import (
+    MultipleRelatedDropdownFilter,
+    RangeDateTimeFilter,
+    RelatedDropdownFilter,
+)
+from unfold.decorators import action
+
+from datetime import datetime
 
 from .models import Barber, Service, Schedule
 
@@ -24,6 +32,40 @@ class MultipleRelatedDropdownFilterAND(MultipleRelatedDropdownFilter):
         return queryset
 
 
+@action(
+    description="Generate barber schedule for next week (Sunday to Saturday, 8 AM - 8 PM)"
+)
+def generate_weekly_schedule(modeladmin, request, queryset):
+    today = now().date()
+
+    # Find upcoming Sunday
+    next_sunday = today + timedelta(days=(6 - today.weekday()) % 7)
+
+    schedules = []
+
+    for barber in queryset:
+        for i in range(7):  # Generate schedules for 7 days (Sunday to Saturday)
+            schedule_date = next_sunday + timedelta(days=i)
+
+            start_time = datetime.combine(schedule_date, datetime.min.time()).replace(
+                hour=8, minute=0, second=0
+            )
+            end_time = start_time + timedelta(hours=12)  # Ends at 8 PM
+            schedules.append(
+                Schedule(
+                    barber=barber, start_datetime=start_time, end_datetime=end_time
+                )
+            )
+
+    # Bulk create schedules
+    Schedule.objects.bulk_create(schedules)
+
+    modeladmin.message_user(
+        request,
+        f"Schedules created for {len(queryset)} barbers from {next_sunday} to {next_sunday + timedelta(days=6)}.",
+    )
+
+
 @register(Barber)
 class BarberAdmin(ModelAdmin):
     list_display = ("first_name", "last_name")
@@ -31,6 +73,8 @@ class BarberAdmin(ModelAdmin):
     list_filter = [
         ("services", MultipleRelatedDropdownFilterAND),
     ]
+
+    actions = [generate_weekly_schedule]
 
 
 @register(Service)
@@ -41,3 +85,9 @@ class ServiceAdmin(ModelAdmin):
 @register(Schedule)
 class ScheduleAdmin(ModelAdmin):
     list_display = ("barber", "start_datetime", "end_datetime")
+
+    list_filter_submit = True
+    list_filter = (
+        ("start_datetime", RangeDateTimeFilter),
+        ("barber", RelatedDropdownFilter),
+    )
