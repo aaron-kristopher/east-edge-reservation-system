f7fe2dacf9d9f8d46349f4a0ec95e23722e5997b
Aaron Lim <aaron.lim.cstr@gmail.com>
2025-02-12 13:44:17 +0800

    feat: Add filtering of barbers via service

--- east_edge_reservation/barbers/admin.py
+++ east_edge_reservation/barbers/admin.py
@@ -1,11 +1,35 @@
 from django.contrib.admin import register
+from django.utils.translation import gettext_lazy as _
 from unfold.admin import ModelAdmin
+from unfold.contrib.filters.admin import MultipleRelatedDropdownFilter
+
 from .models import Barber, Service
+
+
+class MultipleRelatedDropdownFilterAND(MultipleRelatedDropdownFilter):
+    title = _("Services (AND)")  # Admin display name
+    parameter_name = "services"
+
+    def queryset(self, request, queryset):
+        selected_services = self.value()  # Get selected service IDs
+
+        if selected_services:
+            # selected_services is already a list, no need to split
+            service_ids = selected_services
+
+            # Filter barbers who have ALL selected services using AND logic
+            for service_id in service_ids:
+                queryset = queryset.filter(services__id=service_id)
+
+        return queryset
 
 
 @register(Barber)
 class BarberAdmin(ModelAdmin):
-    pass
+    list_filter_submit = True
+    list_filter = [
+        ("services", MultipleRelatedDropdownFilterAND),
+    ]
 
 
 @register(Service)
